4e402f5edd644197b1accb567f1988a9d25320c5
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-07-05 06:40:19 +0530

    added project section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,8 @@
 import { RevealOnScroll } from "../RevealOnScroll";
-import restauImg from "../../assets/restau.png";
+import restauImg from "../../assets/restau.png"; 
+import WebPageImg from "../../assets/webpage.png"; 
+import { VscGithub } from "react-icons/vsc";
+import { TbExternalLink } from "react-icons/tb";
 
 export const Projects = () => {
   return (
@@ -13,12 +16,20 @@ export const Projects = () => {
                 {/* Proj Desc */}
                 <div className="text-xl font-semibold">Dominoza</div>
                 <div className="font-normal pt-2">A Full Stack food ordering platform to list your foods and grow business and Great place for food savours to order and get it at their doorstep.</div>
-
+                <div className="flex space-x-4 p-3 opacity-0 transition-all ease-in duration-100 group-hover:opacity-100">
+                  <a rel="stylesheet" href="https://github.com/Prasanna-spd/Dominoza">
+                    <VscGithub className="text-4xl" />
+                  </a>
+                  <a rel="stylesheet" href="https://dominoza.vercel.app/">
+                    <TbExternalLink className="text-4xl" />
+                  </a>
+                </div>
                 <ul className="mt-auto flex flex-wrap gap-2 text-sm">
-                {["React","Next.js","Tailwind","Stripe","Zustand","Typescript"].map((tech,key)=>(
-                  <li key={key} className="flex cursor-default rounded-lg bg-white p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700">{tech}</li>
-                ))}
-                  
+                  {["React", "Next.js", "Tailwind", "Stripe", "Zustand", "Typescript"].map((tech, key) => (
+                    <li key={key} className="flex cursor-default rounded-lg bg-white p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700">
+                      {tech}
+                    </li>
+                  ))}
                 </ul>
               </div>
 
@@ -30,38 +41,66 @@ export const Projects = () => {
                 absolute -right-44 bottom-10 w-[18rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 transform group-hover:-translate-x-12 group-hover:translate-y-1 group-hover:rotate-0 sm:-right-40 sm:top-9 sm:w-[28rem]"
               />
             </div>
-            <div className="border border-white group relative h-72 w-full lg:w-3/5 overflow-x-hidden rounded-lg  p-5 leading-relaxed shadow-xl transition-all duration-300 odd:self-start even:self-end">
-              <div className="flex h-full w-[80%] flex-col sm:w-[45%]">
+            <div className="group relative h-72 w-full lg:w-3/5 overflow-x-hidden rounded-lg  p-5 leading-relaxed shadow-xl transition-all duration-300 odd:self-start even:self-end border border-white/10 hover:border-white/30 hover:shadow-[0_2px_12px_rgba(255,255,255,0.2)] hover:scale-[1.02]">
+              <div className="flex h-full w-[60%] flex-col sm:w-[45%]">
                 {/* Proj Desc */}
-                <div className="text-xl font-semibold">Dominoza</div>
-                <div className="font-normal">A Full Stack food ordering platform to list your foods and grow business and Great place for food savours to order and get it at their doorstep.</div>
+                <div className="text-xl font-semibold">Web Genie</div>
+                <div className="font-normal pt-2">A AI powered website builder where u can built ur dream website . Choose from a wide varity of templates and go live.</div>
+                <div className="flex space-x-4 p-3 opacity-0 transition-all ease-in duration-100 group-hover:opacity-100">
+                  <a rel="stylesheet" href="https://github.com/Prasanna-spd/Dominoza">
+                    <VscGithub className="text-4xl" />
+                  </a>
+                  <a rel="stylesheet" href="https://github.com/Prasanna-spd/Dominoza">
+                    <TbExternalLink className="text-4xl" />
+                  </a>
+                </div>
+                <ul className="mt-auto flex flex-wrap gap-2 text-sm">
+                  {["React", "Next.js", "LLMs", "AI", "Typescript"].map((tech, key) => (
+                    <li key={key} className="flex cursor-default rounded-lg bg-white p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700">
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* Proj image */}
+              <img
+                src={WebPageImg}
+                alt="WebPageImg"
+                className=" 
+                absolute -right-44 bottom-10 w-[18rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 transform group-hover:-translate-x-12 group-hover:translate-y-1 group-hover:rotate-0 sm:-right-40 sm:top-9 sm:w-[28rem]"
+              />
+            </div>
+            <div className="group relative h-72 w-full lg:w-3/5 overflow-x-hidden rounded-lg  p-5 leading-relaxed shadow-xl transition-all duration-300 odd:self-start even:self-end border border-white/10 hover:border-white/30 hover:shadow-[0_2px_12px_rgba(255,255,255,0.2)] hover:scale-[1.02]">
+              <div className="flex h-full w-[60%] flex-col sm:w-[45%]">
+                {/* Proj Desc */}
+                <div className="text-xl font-semibold">What&pos;s Up</div>
+                <div className="font-normal pt-2">A Real-Time messaging platform where u can connect with ur friends and start talking. A complete solution for your communication.</div>
+                <div className="flex space-x-4 p-3 opacity-0 transition-all ease-in duration-100 group-hover:opacity-100">
+                  <a rel="stylesheet" href="https://github.com/Prasanna-spd/Dominoza">
+                    <VscGithub className="text-4xl" />
+                  </a>
+                  <a rel="stylesheet" href="https://github.com/Prasanna-spd/Dominoza">
+                    <TbExternalLink className="text-4xl" />
+                  </a>
+                </div>
+                <ul className="mt-auto flex flex-wrap gap-2 text-sm">
+                  {["React", "Node.js", "MongoDB", "Socket.io", "Redux", "Typescript"].map((tech, key) => (
+                    <li key={key} className="flex cursor-default rounded-lg bg-white p-1 font-semibold text-gray-200 transition-all duration-300 hover:bg-gray-800 dark:bg-gray-800 dark:hover:bg-gray-700">
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
               </div>
 
               {/* Proj image */}
               <img
                 src={restauImg}
                 alt="RestaurantImg"
-                className=" border border-red-600   
+                className=" 
                 absolute -right-44 bottom-10 w-[18rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 transform group-hover:-translate-x-12 group-hover:translate-y-1 group-hover:rotate-0 sm:-right-40 sm:top-9 sm:w-[28rem]"
               />
             </div>
-            <div className="border border-white group relative h-72 w-full lg:w-3/5 overflow-x-hidden rounded-lg  p-5 leading-relaxed shadow-xl transition-all duration-300 odd:self-start even:self-end">
-              <div className="flex h-full w-[80%] flex-col sm:w-[45%]">
-                {/* Proj Desc */}
-                <div className="text-xl font-semibold">Dominoza</div>
-                <div className="font-normal">A Full Stack food ordering platform to list your foods and grow business and Great place for food savours to order and get it at their doorstep.</div>
-              </div>
-
-              {/* Proj image */}
-              <img
-                src={restauImg}
-                alt="RestaurantImg"
-                className=" border border-red-600   
-                absolute -right-44 bottom-10 w-[18rem] rotate-3 rounded-lg shadow-2xl transition-all duration-300 transform group-hover:-translate-x-12 group-hover:translate-y-1 group-hover:rotate-0 sm:-right-40 sm:top-9 sm:w-[28rem]"
-              />
-            </div>
-            
-            
             
           </div>
         </div>
