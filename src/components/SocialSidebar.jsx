c5a7d18f28d86cdc91dfddb75635d92035f9af62
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-07-07 10:26:46 +0530

    responsive view

--- src/components/SocialSidebar.jsx
+++ src/components/SocialSidebar.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaFileAlt } from "react-icons/fa";
 export const SocialSidebar = () => {
   return (
    
-      <div className="fixed top-1/3 right-7 z-50 flex flex-col items-center space-y-6 p-2 bg-gray-900 border border-white/50 rounded-2xl hover:border-white/30 hover:shadow-[0_2px_12px_rgba(255,255,255,0.2)] hover:scale-[1.02] border-glow">
+      <div className="fixed top-1/3 right-2 space-y-4 p-1 md:space-y-6 md:p-2 md:right-7 z-50 opacity-90 md:opacity-100 scale-90 md:scale-100 flex flex-col items-center bg-gray-900 border border-white/50 rounded-2xl hover:border-white/30 hover:shadow-[0_2px_12px_rgba(255,255,255,0.2)] hover:scale-[1.02] border-glow">
         <a href="https://github.com/Prasanna-spd" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
           <FaGithub size={40} />
         </a>
